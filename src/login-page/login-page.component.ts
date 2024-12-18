c887a9fa1c6501f79e98ef333e39b2ae72123d7f
Aditya Lath <lath2411@gmail.com>
2024-12-19 03:45:41 +0530

    Login page

---
+++ src/login-page/login-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '../app/app-routing.module';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  @ViewChild('username') username!: ElementRef;
+  @ViewChild('password') password!: ElementRef;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  submit() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  change(event: Event) {
+    this.restrictSubmit();
+  }
+
+  restrictSubmit() {
+    if (!this.password || !this.username)
+      return true;
+    else
+      return this.username?.nativeElement?.value === '' || this.password?.nativeElement?.value === '';
+  }
+}
